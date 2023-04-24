10756c6c78b8ee9298a3f4806f26b99e3b0d8fdf
Stefan <stefan@gigmade.com>
2023-04-24 11:32:17 +0200

    Rewrite useConsensus in ts. Remove unused and unreferenced useDistinct.

--- src/useConsensus.ts
+++ src/useConsensus.ts
@@ -1,9 +1,15 @@
 import { useState, useEffect, useMemo, useCallback } from 'react'
 
+type TISet = {
+  add: (item: any) => TISet
+  delete: (item: any) => TISet
+  size: number
+}
+
 /*
  * Poor man's immutable set implementation.
  */
-function ISet() {
+function ISet(): TISet {
   const set = new Set()
 
   function add(item) {
@@ -23,9 +29,9 @@ function ISet() {
   return newSet()
 }
 
-export default function useConsensus(sharedIds = false) {
+export default function useConsensus(sharedIds: boolean = false) {
   // Wrap in { set } to make the set addition and deletion behave like immutable.
-  const [disagreementIds, setLockedIds] = useState(ISet())
+  const [disagreementIds, setLockedIds] = useState<TISet>(ISet())
 
   // `sharedIds` is a static parameter and shouldn't change during consensus
   // negotiation. If it does, we reset the consensus first.
@@ -33,12 +39,12 @@ export default function useConsensus(sharedIds = false) {
     setLockedIds(ISet())
   }, [sharedIds])
 
-  const disagreement = useMemo(() => {
+  const disagreement: boolean = useMemo(() => {
     return disagreementIds.size > 0
   }, [disagreementIds])
 
   const disagree = useCallback(
-    (id) => {
+    (id: any) => {
       // Just make all clientIds unique unless sharedIds = true
       // We still add an optional 'reason' for debugging.
       const uniqueId = sharedIds ? id : { reason: id }
