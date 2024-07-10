f4ac0ba620675daee7b4d516db564a1e8f7d851e
Stefan <stefan@gigmade.com>
2024-07-10 10:34:53 +0200

    Remove isEqual from here because the react-hooks dep is not suitable for use in backends.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export { default as useConsensus } from './useConsensus'
 export { default as useEventCallback } from './useEventCallback'
 export { default as useDebouncedIsEqual } from './useDebouncedIsEqual'
-export * from './isEqual'
 export * from './useResizeObserver'
