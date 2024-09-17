18eed1ad6a6b8889bc58e98f55fb9988721fbe73
Stefan <stefan@gigmade.com>
2024-09-17 16:21:36 +0200

    Move isEqual to separate package

--- src/useDebouncedIsEqual.ts
+++ src/useDebouncedIsEqual.ts
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { isEqual } from './isEqual'
+import { isEqual } from '@gigmade/utils'
 import { useDebouncedCallback } from 'use-debounce'
 
 export default function useDebouncedIsEqual(
